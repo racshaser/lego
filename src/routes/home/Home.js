7b4acfe064bbc5a137de3720ab3878e513b258fb
Andrew Kostin <racshaser@gmail.com>
2020-09-06 11:45:46 +0300

    new stuff

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,6 +3,7 @@ import withStyles from 'isomorphic-style-loader/withStyles';
 import Layout from "../../components/Layout/Layout";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import WeakOverview from "../../components/WeakOverview/WeakOverview";
+import RecentExpenses from "../../components/RecentExpenses/RecentExpenses";
 import styles from './Home.css';
 
 class Home extends Component {
@@ -10,7 +11,10 @@ class Home extends Component {
     return (
      <Layout>
        <SectionTitle left="Overview" />
-       <WeakOverview />
+       <div className={styles.overview}>
+         <WeakOverview />
+         <RecentExpenses />
+       </div>
      </Layout>
     )
   }
